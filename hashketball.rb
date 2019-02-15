16488e6d08237fef07eb94801bad38d8ab81cbdf
OSCAR R <oscr.rss@gmail.com>
2019-02-15 20:20:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,6 @@ def num_points_scored (name)
   game_hash.each do |team_position, team_data|
     team_data[:players].each do |player_name, player_data|
       if player_name == name
-              binding.pry
         return player_data[:points]
      
 end
